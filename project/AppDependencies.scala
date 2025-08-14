d19c0339526588504687cb9e2540bcae43db9520
Byasar3 <byasar3@users.noreply.github.com>
2025-08-14 11:26:29 +0100

    DL-17036: Fix vulnerabilities and enable unsecure http sessions for local performance testing

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,15 +2,15 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "9.13.0"
+  val bootstrapVersion = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.6.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.8.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalamock"     %% "scalamock"              % "7.3.2",
+    "org.scalamock"     %% "scalamock"              % "7.4.1",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatestplus" %% "mockito-5-12"           % "3.2.19.0",
     "org.mockito"       %  "mockito-core"           % "5.18.0"
