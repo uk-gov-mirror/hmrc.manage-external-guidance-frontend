382c8d180956529460a2683e947c1a91c6774283
preetampotdar <10691333+preetampotdar@users.noreply.github.com>
2025-09-26 19:52:39 +0100

    chore(17425): Updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import sbt.*
 
 object AppDependencies {
@@ -6,14 +22,14 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.8.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.15.0"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "org.scalamock"     %% "scalamock"              % "7.4.1",
+    "org.scalamock"     %% "scalamock"              % "7.5.0",
     "uk.gov.hmrc"       %% "bootstrap-test-play-30" % bootstrapVersion,
     "org.scalatestplus" %% "mockito-5-12"           % "3.2.19.0",
-    "org.mockito"       %  "mockito-core"           % "5.18.0"
+    "org.mockito"       %  "mockito-core"           % "5.20.0"
   ).map(_ % Test)
 
   val itDependencies: Seq[ModuleID] = Seq()
