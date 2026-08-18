669265ddeaa77c64026c0f11d2368ef669406e7d
Jayasree Atmakuru <7058921+jayasreemca@users.noreply.github.com>
2026-08-18 10:42:44 +0100

    DL-20053: Update manage-external-guidance-frontend play-frontend-hmrc to version 13 (#216)
    * DL-20053: Update libs
    
    * DL-20053: Update libs version
    
    * DL-20053: Play lib version changed t0 12.32.0
    
    * DL-20053: Upgraded FE version
    
    ---------
    
    Co-authored-by: nehakannaujia1 <50657531+nehakannaujia1@users.noreply.github.com>

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,11 +18,11 @@ import sbt.*
 
 object AppDependencies {
 
-  val bootstrapVersion = "10.7.0"
+  val bootstrapVersion = "10.8.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "12.32.0"
+    "uk.gov.hmrc" %% "play-frontend-hmrc-play-30" % "13.11.0"
   )
 
   val test: Seq[ModuleID] = Seq(
